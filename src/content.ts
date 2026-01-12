6c0b15af2198234bd1714f5a6eddee76dc63551a
mujurin1 <mujurin25@yahoo.co.jp>
2026-01-12 18:05:03 +0900

    マウスホバー周りの動作改善

--- src/content.ts
+++ src/content.ts
@@ -7,20 +7,54 @@ if (document.readyState === "loading") {
   initialize();
 }
 
-let embedIframe: HTMLIFrameElement | null = null;
-
 function initialize(): void {
   document.addEventListener("mouseover", handleMouseOver);
-  document.addEventListener("mouseout", handleMouseOut);
   document.addEventListener("mousedown", handleMouseDown);
 }
 
+const hoverdDelayTimeMs = 200;
+
+//#region インターフェース
+/**
+ * ホバーして iframe を作成前
+ */
+interface HoverState_Before {
+  state: "before";
+  target: HTMLAnchorElement;
+  cancel(): void;
+}
+
+/**
+ * ホバーして iframe が作成されたら
+ */
+interface HoverState_Created {
+  state: "created";
+  isDeleteScheduled: boolean;
+  target: HTMLAnchorElement;
+  iframe: HTMLIFrameElement;
+  destroy(): void;
+}
+
+type HoverState = HoverState_Before | HoverState_Created;
+//#endregion インターフェース
+
+let hoveredState: HoverState | null = null;
+
+
 //#region イベントハンドラ
+function handleMouseOver(e: MouseEvent): void {
+  if (
+    hoveredState == null ||
+    hoveredState.state === "created" && hoveredState.isDeleteScheduled
+  ) createOnHover(e);
+  else destroyOnHover(e);
+}
+
 function handleMouseDown(): void {
   removeIframe();
 }
 
-function handleMouseOver(e: MouseEvent): void {
+function createOnHover(e: MouseEvent): void {
   if (!(e.target instanceof HTMLAnchorElement)) return;
   const url = createUrl(e.target.href);
   if (url == null) return;
@@ -28,30 +62,73 @@ function handleMouseOver(e: MouseEvent): void {
   if (!isTarget(e.target)) return;
   removeIframe();
 
-  embedIframe = createEmbedIframe(url, e.pageX + 5, e.pageY + 5);
-  document.body.appendChild(embedIframe);
+  const rect = e.target.getBoundingClientRect();
+  const x = window.pageXOffset + rect.x;
+  const y = window.pageYOffset + rect.y + rect.height;
+
+  const timerId = setTimeout(() => {
+    const iframe = createEmbedIframe(url, x, y);
+    document.body.appendChild(iframe);
+
+    hoveredState = {
+      state: "created",
+      isDeleteScheduled: false,
+      target: e.target as HTMLAnchorElement,
+      iframe,
+      destroy: () => iframe.parentElement?.removeChild(iframe),
+    };
+  }, hoverdDelayTimeMs);
+
+  hoveredState = {
+    state: "before",
+    target: e.target as HTMLAnchorElement,
+    cancel: () => clearTimeout(timerId),
+  };
 }
 
-function handleMouseOut(): void {
-  if (otherSetting.removeTimer === 0) {
-    removeIframe();
-    return;
+function destroyOnHover(e: MouseEvent): void {
+  if (hoveredState == null) return;
+
+  switch (hoveredState.state) {
+    case "before": {    // iframe の作成を解除する
+      if (e.target === hoveredState.target) return;
+      removeIframe();
+      break;
+    }
+    case "created": {   // 作成した iframe を削除する
+      if (e.target === hoveredState.target) return;
+      if (e.target === hoveredState.iframe) return;
+
+      // 即時削除
+      if (otherSetting.removeTimer === 0) {
+        removeIframe();
+        return;
+      }
+
+      // 一定時間後に削除
+      hoveredState.isDeleteScheduled = true;
+      const target = hoveredState;
+      setTimeout(() => {
+        if (target !== hoveredState) return;
+        removeIframe();
+      }, otherSetting.removeTimer * 1000);
+      break;
+    }
   }
-
-  const target = embedIframe;
-  setTimeout(() => {
-    if (target !== embedIframe) return;
-    removeIframe();
-  }, otherSetting.removeTimer * 1000);
-
 }
 //#endregion イベントハンドラ
 
 
 function removeIframe(): void {
-  if (embedIframe == null) return;
-  embedIframe.parentElement?.removeChild(embedIframe);
-  embedIframe = null;
+  if (hoveredState == null) return;
+
+  if (hoveredState.state === "before") {
+    hoveredState.cancel();
+  } else {
+    hoveredState.destroy();
+  }
+
+  hoveredState = null;
 }
 
 function createEmbedIframe(url: string, x: number, y: number): HTMLIFrameElement {
@@ -80,4 +157,3 @@ function isTarget(target: HTMLAnchorElement): boolean {
 
   return false;
 }
-
